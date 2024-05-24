452c72713f3e28a6076bee93cf0e0f841ac2656b
Chetan Godhani <chetangodhani9@gmail.com>
2024-05-24 00:57:04 -0400

    WIP: user api feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from "@nestjs/common";
 import { User } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
 
   signin() {
-    return {
-      message: "signin"
-    }
+    // Find the user by email
+
+    // If the user is not found, throw an error
   }
 
-  signup() {
-    return {
-      message: "signup"
-    }
+  async signup(data: AuthDto) {
+    // Hash the password
+    const hashedPassword = await argon.hash(data.password);
+
+    // Create a new user
+    const user = await this.prismaService.user.create({
+      data: {
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    delete user.password;
+
+    // Return the user
+    return user;
   }
 }
